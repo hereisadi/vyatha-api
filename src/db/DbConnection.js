bc8916fdb24c3eee5ab11e21dec0c8b639524d3e
hereisadi <hereisadi@proton.me>
2023-09-19 02:49:30 +0530

    setup lint, prettier

--- src/db/DbConnection.js
+++ src/db/DbConnection.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 
 const connectToDb = async () => {
   try {
-    console.log("trying to connect to mongodb...")
+    console.log("trying to connect to mongodb...");
     await mongoose.connect(process.env.MONGODBSECRET, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("MongoDb database connected");
   } catch (error) {
-    console.error(`Establishing connection to Database encountered some error: ${error}`)
+    console.error(
+      `Establishing connection to Database encountered some error: ${error}`
+    );
   }
 };
 
