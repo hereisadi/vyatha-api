f27146cb5c866584184f7e1faf9f74660c1f965c
hereisadi <hereisadi@proton.me>
2023-12-31 06:35:53 +0530

    FIX:some minor bugs

--- src/app.js
+++ src/app.js
@@ -12,8 +12,9 @@ const { redirect } = require("./middlewares/Redirect");
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(cors());
-app.use(bodyParser.json({ limit: "5mb" }));
-app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
+require("dotenv").config();
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 connectToDB();
 
