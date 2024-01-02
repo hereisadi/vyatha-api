5835cfdb3a5c210c7199d291ac05556a895fb803
hereisadi <hereisadi@proton.me>
2024-01-02 17:33:57 +0530

    minor fix in app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
-const app = express();
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const connectToDB = require("./db/DbConnection");
 const { redirect } = require("./middlewares/Redirect");
-
+const app = express();
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(cors());
-app.use(bodyParser.json({ limit: "10mb" }));
-require("dotenv").config();
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 connectToDB();
 
