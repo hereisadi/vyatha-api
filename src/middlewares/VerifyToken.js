74ce1a5f724e4262890b83b28f22c842baa25196
hereisadi <hereisadi@proton.me>
2024-01-09 12:17:21 +0530

    WIP:jwt malformed error

--- src/middlewares/VerifyToken.js
+++ src/middlewares/VerifyToken.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 
 const verifyToken = async (req, res, next) => {
   try {
-    const token = req?.headers?.authorization;
+    const token = req.headers.authorization;
 
     if (!token) {
+      console.error("Missing token");
       return res.status(401).json({ error: "Missing token" });
     }
+
     const decoded = jwt.verify(
       token.split(" ")[1],
       process.env.YOUR_SECRET_KEY
@@ -20,7 +22,9 @@ const verifyToken = async (req, res, next) => {
     next();
   } catch (error) {
     console.error("error in verifying the token", error);
-    res.status(401).json({ error: "Something went wrong to verify the token" });
+    return res
+      .status(401)
+      .json({ error: "Something went wrong to verify the token" });
   }
 };
 
